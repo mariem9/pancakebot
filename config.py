8cd15103e86e88eaba215f0a5c29e1260cc04dc8
unknown <mariem9>
2022-07-08 19:14:34 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+YOUR_WALLET_ADDRESS = os.environ.get('0x09eC59cAB36Be70E967Df7268073aEDD43829C9D')  # Add Your Wallet Address here by removing whole line
+YOUR_PRIVATE_KEY = os.environ.get('d1f463c54b8d84bf056a9e512aab379d81afa18cf71e96a7459cbf516789ac5d')  # Add Your Private Key here by removing whole line
+
+TRADE_TOKEN_ADDRESS = None  # Add token address here example : "0xc66c8b40e9712708d0b4f27c9775dc934b65f0d9"
+WBNB_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+PANCAKE_ROUTER_ADDRESS = "0x10ED43C718714eb63d5aA57B78B54704E256024E"
+SHOW_TX_ON_BROWSER = True
+
+SELL_TOKENS = None
+BUY_TOKENS = None
